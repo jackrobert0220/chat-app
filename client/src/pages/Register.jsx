e3d6393537b3c6345adaf6a04a86a0453b2b9933
jackrobert0220 <jackrobert0220@gmail.com>
2022-04-19 22:36:33 -0700

    setting up register page with initial event handlers

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import styled from 'styled-components';
 
 function Register() {
+
+    const handleSubmit = (event)=> {
+        event.preventDefault();
+        alert("form");
+    };
+
+    const handleChange = (event) => {};
   return (
-    <div>Register</div>
+    <>
+        <FormContainer>
+            <form onSubmit={(event)=>handleSubmit(event)}>
+                <div className="brand">
+                    <img src="" alt="" />
+                    <h1>Chat App</h1>
+                </div>
+                <input type="text" placeholder="Username" name="username" onChange={(e) => handleChange(e)} />
+            </form>
+        </FormContainer>
+    </>
   )
 }
+
+const FormContainer = styled.div``;
 
 export default Register
