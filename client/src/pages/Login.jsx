b6cf239fb8422d644c69421d3c1d7263ad242b6a
jackrobert0220 <jackrobert0220@gmail.com>
2022-04-23 14:56:51 -0700

    login route setup

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,189 @@
-import React from 'react'
+import React, { useState, } from 'react';
+import styled from 'styled-components';
+import { Link, useNavigate } from 'react-router-dom';
+import Logo from "../assets/logo.png";
+import Background from "../assets/background.jpg";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { registerRoute } from '../utils/APIRoutes';
 
 function Login() {
+    const navigate = useNavigate()
+    const [values, setValues] = useState({
+        username: "",
+        email: "",
+        password: "",
+        confirmpassword: "",
+    });
+
+    const toastOptions = {
+        position: "bottom-right",
+        autoClose: 8000,
+        pauseOnHover: true,
+        draggable: true,
+        theme: "dark",
+    }
+
+    const handleSubmit = async (event)=> {
+        event.preventDefault();
+        if(handleValidation()) {
+        const { password, username, email } = values;
+            const { data } = await axios.post(registerRoute, {
+                username,
+                email,
+                password,
+            });
+
+            if(data.status === false) {
+                toast.error(data.msg, toastOptions);
+            }
+            if(data.status === true) {
+                localStorage.setItem('chat-app-user', JSON.stringify(data.user));
+                navigate("/");
+            }
+        }
+    };
+
+    const handleValidation = () => {
+        const { password, confirmPassword, username, email } = values;
+        if (password !== confirmPassword) {
+            toast.error(
+                "Password and Confirm Password do not match.",
+                toastOptions
+            );
+            return false;
+        } else if (username.length < 3) {
+            toast.error("Username has to be greater than 3 characters", toastOptions);
+            return false;
+        } else if (password.length < 8) {
+            toast.error("Password has to be 8 characters or more", toastOptions);
+            return false;
+        } else if (email === "") {
+            toast.error("Email is required", toastOptions);
+        }
+        return true;
+    };
+
+    const handleChange = (event) => {
+        setValues({...values, [event.target.name]: event.target.value })
+    };
+
   return (
-    <div>Login</div>
+    <>
+        <FormContainer>
+            <form onSubmit={(event)=>handleSubmit(event)}>
+                <div className="brand">
+                    <img src={Logo} alt="LOgo" />
+                    <h1>Chat App</h1>
+                </div>
+                <input 
+                    type="text" 
+                    placeholder="Username" 
+                    name="username" 
+                    onChange={(e) => handleChange(e)} 
+                />
+                <input 
+                    type="email" 
+                    placeholder="Email" 
+                    name="email" 
+                    onChange={(e) => handleChange(e)} 
+                />
+                <input 
+                    type="password" 
+                    placeholder="Password" 
+                    name="password" 
+                    onChange={(e) => handleChange(e)} 
+                />
+                <input 
+                    type="password" 
+                    placeholder="Confirm Password" 
+                    name="confirmPassword" 
+                    onChange={(e) => handleChange(e)} 
+                />
+                <button type="submit">Create User</button>
+                <span>
+                Already have an account? <Link to="/login">Login</Link>
+                </span>             
+            </form>
+        </FormContainer>
+        <ToastContainer />
+    </>
   )
 }
+
+const FormContainer = styled.div`
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 1rem;
+    align-items: center;
+    background-image: url(${Background});
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    .brand {
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+        justify-content: center;
+        img {
+            height: 7rem;
+            border-radius: 0.4rem;
+        }
+        h1 {
+            color: white;
+            text-transform: uppercase;
+        }
+    }
+    form {
+        display: flex;
+        flex-direction: column;
+        gap: 2rem;
+        background-color: #00000076;
+        border-radius: 2rem;
+        padding: 3rem 5rem;
+        input {
+            background-color: transparent;
+            padding: 1rem;
+            border: 0.1rem solid #0BDA51;
+            border-radius: 0.4rem;
+            color: white;
+            width: 100%;
+            font-size: 1rem;
+            &:focus {
+                border: 0.1rem solid #ECFFDC;
+                outline: none;
+            }
+        }
+        button {
+            background-color: #0BDA51;
+            color: white;
+            padding: 1rem 2rem;
+            border: none;
+            font-weight: bold;
+            cursor: pointer;
+            border-radius: 0.4rem;
+            font-size: 1rem;
+            text-transform: uppdercase;
+            transition: 0.5s ease-in-out;
+            &:hover {
+                background-color: #90EE90;
+            }
+            }
+            span {
+                color: white;
+                text-transform: uppercase;
+                a {
+                    color: #90EE90;
+                    text-decoration: none;
+                    font-weight: bold;
+                }
+            }
+        }
+    }
+`;
 
 export default Login
