e09bc13633059672dbbc15401f1c78be576dc76a
jackrobert0220 <jackrobert0220@gmail.com>
2022-04-23 13:52:02 -0700

    register connectivity working

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
+const userRoutes = require("./routes/userRoutes")
 
 const app = express();
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", userRoutes)
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -17,6 +20,6 @@ mongoose.connect(process.env.MONGO_URL, {
     console.log(err.message);
 })
 
-const server = app.listen(process.env.PORT,()=> {
+const server = app.listen(process.env.PORT,() => {
     console.log(`Server started on Port ${process.env.PORT}`)
 });
