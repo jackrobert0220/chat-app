cfca3363b362488644c06420f133de26a476a0cd
jackrobert0220 <jackrobert0220@gmail.com>
2022-04-23 17:14:40 -0700

    working on useffect for setavatar

--- client/src/pages/SetAvatar.jsx
+++ client/src/pages/SetAvatar.jsx
@@ -7,15 +7,59 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
 import { setAvatarRoute } from '../utils/APIRoutes';
+import { Buffer } from 'buffer';
 
 export default function SetAvatar() {
     const api = "https://api.multiavatar.com/45678945";
     const navigate = useNavigate();
+    const [avatars, setAvatars] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [selectedAvatar, setSelectedAvatar] = useState(undefined);
+    const toastOptions = {
+        position: "bottom-right",
+        autoClose: 8000,
+        pauseOnHover: true,
+        draggable: true,
+        theme: "dark",
+    };
+
+const setProfilePicture = async () => {};
+
+useEffect(() => {
+    const data = [];
+    for (let i = 0; i < 4; i++) {
+      const image = await axios.get(
+        `${api}/${Math.round(Math.random() * 1000)}`
+      );
+      const buffer = new Buffer(image.data);
+      data.push(buffer.toString("base64"));
+    }
+    setAvatars(data);
+    setIsLoading(false);
+  }, []);
 
   return(
   <>
-    <Container>SetAvatar</Container>;
+    <Container>
+        <div className="title-container">
+            <h1>Pick an avatar as your profile picture</h1>
+        </div>
+        <div className="avatars">
+            {avatars.map((avatar, index) => {
+                    return (
+                        <div key={index} 
+                            className={`avatar ${selectedAvatar === index ?"selected":""}`}>
+
+                            <img src={`data:image/svg+xml;base64,${avatar}`} alt="avatar"
+                            onClick={() => setSelectedAvatar(index)} />
+
+                        </div>
+                    )
+                })
+            }
+        </div>
     <ToastContainer />
+    </Container>
   </>
   )
 }
