878398bc45bc9a8fa47a60c6d39f34f9b4e74293
jackrobert0220 <jackrobert0220@gmail.com>
2022-04-25 17:20:19 -0700

    building out contacts component

--- client/src/components/Contacts.jsx
+++ client/src/components/Contacts.jsx
@@ -1,8 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import styled from "styled-components";
+import logo from "../assets/logo.png";
 
 export default function Contacts({contacts, currentUser}) {
+    const [currentUserName, setCurrentUserName] = useState(undefined);
+    const [currentUserImage, setCurrentUserImage] = useState(undefined);
+    const [currentSelected, setCurrentSelected] = useState(undefined);
+    
+    useEffect(() => {
+        if(currentUser) {
+            setCurrentUserImage(currentUser.avatarImage);
+            setCurrentUserName(currentUser.username);
+        }
+    }, [currentUser]);
+    const changeCurrentChat = (index, contact) => {};
+
   return (
-    <div>Contacts</div>
-  )
-}
+    <>
+        {currentUserImage && currentUserName && (
+            <Container>
+                <div className="brand">
+                    <img src={logo} alt="logo"/>
+                    <h3>Chat App</h3>
+                </div>
+                <div className="contacts">
+                    {
+                        contacts.map((contact,index) => {
+                            return (
+                                <div className={`contact ${index === currentSelected ?"selected":""}`} key={index} >
+                                    <div className="avatar">
+                                        <img src={`data:image/svg+xml;base64,${contact.avatarImage}`} alt="avatar"/>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </Container>
+    )}
+</>
+)}
+
+const Container = styled.div;
 
