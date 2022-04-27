1b9026cf20975ae833977691ee662ac94feb6903
jackrobert0220 <jackrobert0220@gmail.com>
2022-04-26 21:49:58 -0700

    logout works

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import Logout from './Logout';
 
 
 export default function ChatContainer({currentChat}) {
@@ -18,6 +19,7 @@ export default function ChatContainer({currentChat}) {
                 <h3>{currentChat.username}</h3>
             </div>
         </div>   
+        <Logout />
     </div>
     <div className="chat-messages"></div>
     <div className="chat-input"></div>
