4f5844f0f939b2a60acbfdb7470a721826117170
jackrobert0220 <jackrobert0220@gmail.com>
2022-04-26 19:04:15 -0700

    welcome component made

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import styled from 'styled-compponents';
-import zombie from "../assets/zombie.gif";
+import styled from 'styled-components';
+import Zombie from "../assets/zombie.gif";
 
 export default function Welcome({ currentUser }) {
   return (
-    <div>Welcome</div>
-  )
+    <Container>
+    <img src={Zombie} alt="Zombie" />
+    <h1>
+        Welcome, <span>{currentUser.username}!</span>
+    </h1>
+    <h3>Please select a chat to Start Messaging.</h3>
+
+    </Container>
+  );
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    color: white;
+    img {
+        height: 10rem;
+    }
+    span {
+        color: #4e00ff;
+    }
+`;
