be09df2b08777728783cdfcc60b66c9cfb3d656b
jackrobert0220 <jackrobert0220@gmail.com>
2022-04-27 18:49:50 -0700

    messageAPI setup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
-const express = require("express")
-const cors = require("cors")
-const mongoose = require("mongoose")
-const userRoutes = require("./routes/userRoutes")
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+const messageRoute = require("./routes/messagesRoute");
 
 const app = express();
 require("dotenv").config();
@@ -9,7 +10,8 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", userRoutes)
+app.use("/api/auth", userRoutes);
+app.use("/api/messages", messageRoute);
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -18,7 +20,7 @@ mongoose.connect(process.env.MONGO_URL, {
     console.log("DB Connection Successfull!");
 }).catch((err)=> {
     console.log(err.message);
-})
+});
 
 const server = app.listen(process.env.PORT,() => {
     console.log(`Server started on Port ${process.env.PORT}`)
