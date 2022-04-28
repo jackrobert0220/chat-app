b297ff7fbf08c0b4a3a560b638e3b6f43e61638c
jackrobert0220 <jackrobert0220@gmail.com>
2022-04-27 21:20:32 -0700

    setup socket.io connection in chat page

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,20 +1,24 @@
-import React, {useState, useEffect } from 'react';
+import React, {useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import Background from "../assets/background.jpg";
 import axios from "axios";
 import { useNavigate } from 'react-router-dom';
-import { allUsersRoute } from "../utils/APIRoutes";
+import { allUsersRoute, host } from "../utils/APIRoutes";
 import Contacts from "../components/Contacts";
 import Welcome from "../components/Welcome";
 import ChatContainer from "../components/ChatContainer";
+import { io } from "socket.io-client";
 
 function Chat() {
+  const socket = useRef();
   const navigate = useNavigate;
   const [contacts, setContacts] = useState([]);
   const [currentUser, setCurrentUser] = useState(undefined);
   const [currentChat, setCurrentChat] = useState(undefined);
   const [isLoaded, setIsLoaded] = useState(false);
 
+
+// Confirms User
   useEffect(() => {
     if(!localStorage.getItem("chat-app-user")) {
       navigate("/login");
@@ -23,17 +27,26 @@ function Chat() {
     setIsLoaded(true);
     // eslint-disable-next-line
   }}, []);
-  
-  useEffect(() => {
 
+
+// Connects User after User is Confirmed
+  useEffect(() => {
+    if(currentUser) {
+      socket.current = io(host);
+      socket.current.emit("add-user", currentUser._id);
+    }
+  }, [currentUser]);
+  
+
+// Checks for User's Avatar
+  useEffect(() => {
     if(currentUser) {
       if(currentUser.isAvatarImageSet) {
         /* Thank you, Gabe and Avisa \m/, (>.<) ,\m/ */
-       axios.get(`${allUsersRoute}/${currentUser._id}`).then((res) => {
-         console.log(res.data)
-         setContacts(res.data)
-       });
-       
+        axios.get(`${allUsersRoute}/${currentUser._id}`).then((res) => {
+          console.log(res.data)
+          setContacts(res.data)
+        }); 
       } else {
         navigate("/setAvatar");
       }
@@ -41,11 +54,10 @@ function Chat() {
   /* Thank you, Rome!!! \m/, (>.<) ,\m/ */
 // eslint-disable-next-line
   }, [currentUser]);
-console.log(contacts);
+
   const handleChatChange = (chat) => {
       setCurrentChat(chat);
   }
-  
   
   return (
     <Container>
@@ -60,7 +72,9 @@ console.log(contacts);
           ) : ( 
             <ChatContainer 
             currentChat={currentChat} 
-            currentUser={currentUser}/>
+            currentUser={currentUser}
+            socket={socket}
+            />
           )}
       </div>
     </Container>
