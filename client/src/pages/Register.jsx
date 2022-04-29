8cd95e6a4ff8731e5fbcf4caea47a78a6a7b0252
jackrobert0220 <jackrobert0220@gmail.com>
2022-04-28 19:37:56 -0700

    fixed issue with avatar

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -8,15 +8,8 @@ import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
 import { registerRoute } from '../utils/APIRoutes';
 
-function Register() {
-    const navigate = useNavigate()
-    const [values, setValues] = useState({
-        username: "",
-        email: "",
-        password: "",
-        confirmpassword: "",
-    });
-
+export default function Register() {
+    const navigate = useNavigate();
     const toastOptions = {
         position: "bottom-right",
         autoClose: 8000,
@@ -24,102 +17,119 @@ function Register() {
         draggable: true,
         theme: "dark",
     };
+    const [values, setValues] = useState({
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+    });
 
     useEffect(() => {
-        if (localStorage.getItem("chat-app-user")) {
+        if (localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
         navigate("/");
         }
-    // eslint-disable-next-line 
-        }, []);
+        // eslint-disable-next-line
+    }, []);
 
-    const handleSubmit = async (event)=> {
-        event.preventDefault();
-        if(handleValidation()) {
-        const { password, username, email } = values;
-            const { data } = await axios.post(registerRoute, {
-                username,
-                email,
-                password,
-            });
-
-            if(data.status === false) {
-                toast.error(data.msg, toastOptions);
-            }
-            if(data.status === true) {
-                localStorage.setItem('chat-app-user', JSON.stringify(data.user));
-                navigate("/");
-            }
-        }
+    const handleChange = (event) => {
+        setValues({ ...values, [event.target.name]: event.target.value });
     };
 
     const handleValidation = () => {
         const { password, confirmPassword, username, email } = values;
         if (password !== confirmPassword) {
-            toast.error(
-                "Password and Confirm Password do not match.",
-                toastOptions
-            );
-            return false;
+        toast.error(
+            "Password and confirm password should be same.",
+            toastOptions
+        );
+        return false;
         } else if (username.length < 3) {
-            toast.error("Username has to be greater than 3 characters", toastOptions);
-            return false;
+        toast.error(
+            "Username should be greater than 3 characters.",
+            toastOptions
+        );
+        return false;
         } else if (password.length < 8) {
-            toast.error("Password has to be 8 characters or more", toastOptions);
-            return false;
+        toast.error(
+            "Password should be equal or greater than 8 characters.",
+            toastOptions
+        );
+        return false;
         } else if (email === "") {
-            toast.error("Email is required", toastOptions);
+        toast.error("Email is required.", toastOptions);
+        return false;
         }
+
         return true;
     };
 
-    const handleChange = (event) => {
-        setValues({...values, [event.target.name]: event.target.value })
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        if (handleValidation()) {
+        const { email, username, password } = values;
+        const { data } = await axios.post(registerRoute, {
+            username,
+            email,
+            password,
+        });
+
+        if (data.status === false) {
+            toast.error(data.msg, toastOptions);
+        }
+        if (data.status === true) {
+            localStorage.setItem(
+            process.env.REACT_APP_LOCALHOST_KEY,
+            JSON.stringify(data.user)
+            );
+            navigate("/setavatar");
+        }
+        }
     };
 
-  return (
-    <>
+    return (
+        <>
         <FormContainer>
-            <form onSubmit={(event)=>handleSubmit(event)}>
-                <div className="brand">
-                    <img src={Logo} alt="LOgo" />
-                    <h1>Chat App</h1>
-                </div>
-                <input 
-                    type="text" 
-                    placeholder="Username" 
-                    name="username" 
-                    onChange={(e) => handleChange(e)} 
-                />
-                <input 
-                    type="email" 
-                    placeholder="Email" 
-                    name="email" 
-                    onChange={(e) => handleChange(e)} 
-                />
-                <input 
-                    type="password" 
-                    placeholder="Password" 
-                    name="password" 
-                    onChange={(e) => handleChange(e)} 
-                />
-                <input 
-                    type="password" 
-                    placeholder="Confirm Password" 
-                    name="confirmPassword" 
-                    onChange={(e) => handleChange(e)} 
-                />
-                <button type="submit">Create User</button>
-                <span>
-                Already have an account? <Link to="/login">Login</Link>
-                </span>             
+            <form action="" onSubmit={(event) => handleSubmit(event)}>
+            <div className="brand">
+                <img src={Logo} alt="logo" />
+                <h1>snappy</h1>
+            </div>
+            <input
+                type="text"
+                placeholder="Username"
+                name="username"
+                onChange={(e) => handleChange(e)}
+            />
+            <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                onChange={(e) => handleChange(e)}
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                onChange={(e) => handleChange(e)}
+            />
+            <input
+                type="password"
+                placeholder="Confirm Password"
+                name="confirmPassword"
+                onChange={(e) => handleChange(e)}
+            />
+            <button type="submit">Create User</button>
+            <span>
+                Already have an account ? <Link to="/login">Login.</Link>
+            </span>
             </form>
         </FormContainer>
         <ToastContainer />
-    </>
-  )
-}
+        </>
+    );
+    }
 
-const FormContainer = styled.div`
+    const FormContainer = styled.div`
     height: 100vh;
     width: 100vw;
     display: flex;
@@ -128,21 +138,17 @@ const FormContainer = styled.div`
     gap: 1rem;
     align-items: center;
     background-image: url(${Background});
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
     .brand {
         display: flex;
         align-items: center;
         gap: 1rem;
         justify-content: center;
         img {
-            height: 7rem;
-            border-radius: 0.4rem;
+        height: 5rem;
         }
         h1 {
-            color: white;
-            text-transform: uppercase;
+        color: white;
+        text-transform: uppercase;
         }
     }
     form {
@@ -152,45 +158,41 @@ const FormContainer = styled.div`
         background-color: #00000076;
         border-radius: 2rem;
         padding: 3rem 5rem;
-        input {
-            background-color: transparent;
-            padding: 1rem;
-            border: 0.1rem solid #0BDA51;
-            border-radius: 0.4rem;
-            color: white;
-            width: 100%;
-            font-size: 1rem;
-            &:focus {
-                border: 0.1rem solid #ECFFDC;
-                outline: none;
-            }
-        }
-        button {
-            background-color: #0BDA51;
-            color: white;
-            padding: 1rem 2rem;
-            border: none;
-            font-weight: bold;
-            cursor: pointer;
-            border-radius: 0.4rem;
-            font-size: 1rem;
-            text-transform: uppdercase;
-            transition: 0.5s ease-in-out;
-            &:hover {
-                background-color: #90EE90;
-            }
-            }
-            span {
-                color: white;
-                text-transform: uppercase;
-                a {
-                    color: #90EE90;
-                    text-decoration: none;
-                    font-weight: bold;
-                }
-            }
+    }
+    input {
+        background-color: transparent;
+        padding: 1rem;
+        border: 0.1rem solid #4e0eff;
+        border-radius: 0.4rem;
+        color: white;
+        width: 100%;
+        font-size: 1rem;
+        &:focus {
+        border: 0.1rem solid #997af0;
+        outline: none;
         }
     }
-`;
-
-export default Register
+    button {
+        background-color: #4e0eff;
+        color: white;
+        padding: 1rem 2rem;
+        border: none;
+        font-weight: bold;
+        cursor: pointer;
+        border-radius: 0.4rem;
+        font-size: 1rem;
+        text-transform: uppercase;
+        &:hover {
+        background-color: #4e0eff;
+        }
+    }
+    span {
+        color: white;
+        text-transform: uppercase;
+        a {
+        color: #4e0eff;
+        text-decoration: none;
+        font-weight: bold;
+        }
+    }
+    `;
