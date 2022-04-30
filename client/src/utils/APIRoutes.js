407f95599e60f3e049d5a0c91e59a59c2c8ce385
jackrobert0220 <jackrobert0220@gmail.com>
2022-04-30 14:58:48 -0700

    reconfigured localhost

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = process.env.HOST;
 export const registerRoute = `${host}/api/auth/register`;
 export const deleteUserRoute = `${host}/api/auth/deleteUser`;
 export const loginRoute = `${host}/api/auth/login`;
